5c94ef8ce9f645ce2db2b6eea7ead9d026d776cd
Yves Devineau <ydhello@gmail.com>
2023-09-20 17:16:17 +0200

    Create faiss_feed.py

---
+++ faiss_feed.py
@@ -0,0 +1,32 @@
+import argparse
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import FAISS
+
+def main(data_path, faiss_save_path):
+    text_loader_kwargs = {'autodetect_encoding': True}
+    loader = DirectoryLoader(data_path, glob="**/*.txt", loader_cls=TextLoader, loader_kwargs=text_loader_kwargs)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
+    all_splits = text_splitter.split_documents(docs)
+
+    model_name = "sentence-transformers/all-MiniLM-L6-v2"
+    model_kwargs = {"device": "cuda"}
+
+    embeddings = HuggingFaceEmbeddings(model_name=model_name, model_kwargs=model_kwargs)
+
+    # storing embeddings in the vector store
+    vectorstore_0 = FAISS.from_documents(all_splits, embeddings)
+
+    # Save the FAISS index
+    vectorstore_0.save(faiss_save_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Load raw data and save FAISS index.")
+    parser.add_argument("data_path", type=str, help="Path to access the raw data.")
+    parser.add_argument("faiss_save_path", type=str, help="Location to save the FAISS index.")
+    
+    args = parser.parse_args()
+    main(args.data_path, args.faiss_save_path)
