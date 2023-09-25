e8735cb2a8a474068ece2efc1d76e94df54ab0ed
Yves Devineau <ydhello@gmail.com>
2023-09-25 15:36:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,28 +24,22 @@ def conversation_chat(query):
 def initialize_session_state():
     if 'history' not in st.session_state:
         st.session_state['history'] = []
-
     if 'generated' not in st.session_state:
         st.session_state['generated'] = ["Hello! Ask me anything about"]
-
     if 'past' not in st.session_state:
         st.session_state['past'] = ["Hey!"]
 
 def display_chat_history():
     reply_container = st.container()
     container = st.container()
-
     with container:
         with st.form(key='my_form', clear_on_submit=True):
             user_input = st.text_input("Question:", placeholder="Ask me about 8A", key='input')
             submit_button = st.form_submit_button(label='Send')
-
         if submit_button and user_input:
             output = conversation_chat(user_input)
-
             st.session_state['past'].append(user_input)
             st.session_state['generated'].append(output)
-
     if st.session_state['generated']:
         with reply_container:
             for i in range(len(st.session_state['generated'])):
