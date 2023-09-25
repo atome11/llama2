2a09db8352d7fe1a76302252013bdc0c10ba1410
Yves Devineau <ydhello@gmail.com>
2023-09-25 16:13:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from torch import cuda, bfloat16
 import transformers
 import yaml
 from yaml.loader import SafeLoader
+from langchain.chains import ConversationalRetrievalChain
 
 
 # sélection du modèle 
@@ -24,6 +25,53 @@ authenticator = stauth.Authenticate(
 # initialisation de l'écran de login
 authenticator.login('Login', 'main')
 
+# Fonctions du modèle
+# --------------------------------------------------------------------------------------------------------------------------------
+def initialize_model(model_id):
+    # set quantization configuration to load large model with less GPU memory
+    # this requires the `bitsandbytes` library
+    bnb_config = transformers.BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_compute_dtype=bfloat16
+    )
+    
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        trust_remote_code=True,
+        #config=model_config,
+        quantization_config=bnb_config,
+        device_map='auto',
+        #use_auth_token=hf_auth
+    )
+    model.eval()
+    print(f"Model loaded on {device}")
+    return model
+
+def tokenizer(hf_auth,model_id):
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_id,
+        use_auth_token=hf_auth
+    )
+    return tokenizer
+
+def generate_text_pipeline(model):
+    generate_text = transformers.pipeline(
+        model=model,
+        tokenizer=tokenizer('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','meta-llama/Llama-2-13b-chat-hf'),
+        return_full_text=True,  # langchain expects the full text
+        task='text-generation',
+        # we pass model parameters here too
+        #stopping_criteria=stopping_criteria,  # without this model rambles during chat
+        temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+        max_new_tokens=4096,  # max number of tokens to generate in the output
+        repetition_penalty=1.1  # without this output begins repeating
+    )
+    return generate_text
+
+# Fonctions du chatbot
+# --------------------------------------------------------------------------------------------------------------------------------
 def conversation_chat(query):
     result = {"answer":"oui ?"}
     st.session_state['history'].append((query, result["answer"]))
